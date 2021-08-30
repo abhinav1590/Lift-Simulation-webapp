4aa6a11882b14bf8acbc46fc263bfc9e961492bb
Abhinav Mishra <86315672+abhinav1590@users.noreply.github.com>
2021-08-30 18:53:46 +0530

    Update stimulation.js

--- stimulation.js
+++ stimulation.js
@@ -5,7 +5,7 @@ floor2 = 276
 floor3 = 155
 floor4 = 33
 pos = floor1;
-function MoveDownFloor1() {
+function MoveToFloor1() {
 
     var id = setInterval(frame, 12);
 
@@ -21,6 +21,7 @@ function MoveDownFloor1() {
 }
 function MoveDownFloor2() {
 
+    if (pos!= floor1){
     var id = setInterval(frame, 12);
   
     function frame() {
@@ -28,24 +29,31 @@ function MoveDownFloor2() {
         if (pos == floor2 ) {
             clearInterval(id);
         } else {
-            if(elem.style.top < floor2 + "px") {
                 pos++;
                 elem.style.top = pos + "px";
-        }   else {
-                    if( elem.style.top < floor4){
-                        pos = floor2
-                        elem.style.top = floor1 + "px";
-                    }
-                    else{
-                pos--;
-                elem.style.top = pos + "px"; }
             }
+        }
     }
-  }
 }
-
+function MoveUpFloor2()
+{
+    if(pos > floor3){
+    var id = setInterval(frame, 12);
+    function frame(){
+            if(pos == floor2){
+                clearInterval(id);
+            }
+            else {
+            
+                pos--;
+                elem.style.top = pos + "px";
+            }
+        }
+   }
+}
 function MoveDownFloor3() {
 
+    if (pos!= floor1 && pos!= floor2){
     var id = setInterval(frame, 12);
   
     function frame() {
@@ -53,23 +61,28 @@ function MoveDownFloor3() {
       if (pos == floor3) {
         clearInterval(id);
       } else {
-        if(elem.style.top < floor3 + "px") {
             pos++;
             elem.style.top = pos + "px";
-        }else  {
-            if( elem.style.top < floor4){
-                pos = floor3
-                elem.style.top = floor3 + "px";
-            } 
-            else{
-            pos--;
-            elem.style.top = pos + "px"; }
-        }
+            }
         }
     }
 }
-
-function MoveDownFloor4()
+function MoveUpFloor3()
+{
+    if(pos > floor4){
+    var id = setInterval(frame, 12);
+    function frame(){
+        if(pos == floor3){
+            clearInterval(id);
+        }
+        else {
+            pos--;
+            elem.style.top = pos + "px";
+            }
+        }
+    }
+}
+function MoveToFloor4()
 {
     var id = setInterval(frame, 12);
     function frame()
