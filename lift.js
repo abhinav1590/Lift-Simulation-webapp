8cd1456295152427ac21385a702abbd785c6d2ba
Abhinav Mishra <86315672+abhinav1590@users.noreply.github.com>
2021-09-04 11:54:31 +0530

    Update lift.js

--- lift.js
+++ lift.js
@@ -4,34 +4,34 @@ const callBtn= document.querySelectorAll('button');
 function moveLift(el){
   switch(el.id){
     case 'level10':
-    lift.style.bottom = '90.5%';
+    lift.style.bottom = '90.8%';
     break;
     case 'level9':
-    lift.style.bottom='81.1%';
+    lift.style.bottom='81.4%';
     break;
     case 'level8':
-    lift.style.bottom='71.5%';
+    lift.style.bottom='71.7%';
     break;
     case 'level7':
-    lift.style.bottom='61.9%';
+    lift.style.bottom='62.2%';
     break;
     case 'level6':
-    lift.style.bottom='52.1%';
+    lift.style.bottom='52.4%';
     break;
     case 'level5':
-    lift.style.bottom='42.5%';
+    lift.style.bottom='42.7%';
     break;
     case 'level4':
-    lift.style.bottom='33.0%';
+    lift.style.bottom='33.4%';
     break;
     case 'level3':
-    lift.style.bottom='23.2%';
+    lift.style.bottom='23.8%';
     break;
     case 'level2':
-    lift.style.bottom='13.5%';
+    lift.style.bottom='14.6%';
     break;
     case 'level1':
-    lift.style.bottom='4%';
+    lift.style.bottom='5.4%';
     break;
   }
 }
