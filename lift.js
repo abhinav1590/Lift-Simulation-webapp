fce70dbc16fe74f6da21d4c7093dac2e67f088fa
Abhinav Mishra <86315672+abhinav1590@users.noreply.github.com>
2021-09-04 17:37:06 +0530

    Update lift.js

--- lift.js
+++ lift.js
@@ -3,12 +3,11 @@ const callBtn= document.querySelectorAll('button');
 const t1 = '1.5s'
 const t2 = '3.0s'
 function moveLift(el){
+  
   var position = parseFloat(lift.style.bottom);
-  if(position >= 62.2 || position <=33.4)
-    lift.style.transitionDuration = t1;
-  else
-    lift.style.transitionDuration= t2;
+  
   switch(el.id){
+      
     case 'level10':
       if(position == 81.4)
       lift.style.transitionDuration = t1;
@@ -16,6 +15,7 @@ function moveLift(el){
       lift.style.transitionDuration= t2;
       lift.style.bottom = '90.8%';
       break;
+      
     case 'level9':
       if(position == 71.7 || position == 90.8)
       lift.style.transitionDuration = t1;
@@ -23,6 +23,7 @@ function moveLift(el){
       lift.style.transitionDuration= t2;
       lift.style.bottom='81.4%';
       break;
+      
     case 'level8':
       if(position == 62.2 || position == 81.4)
       lift.style.transitionDuration = t1;
@@ -30,6 +31,7 @@ function moveLift(el){
       lift.style.transitionDuration= t2;
       lift.style.bottom='71.7%';
       break;
+      
     case 'level7':
       if(position == 52.4 || position == 71.7)
       lift.style.transitionDuration = t1;
@@ -37,6 +39,7 @@ function moveLift(el){
       lift.style.transitionDuration= t2;
       lift.style.bottom='62.2%';
       break;
+      
     case 'level6':
       if(position == 42.7 || position == 62.2)
       lift.style.transitionDuration = t1;
@@ -44,6 +47,7 @@ function moveLift(el){
       lift.style.transitionDuration= t2;
       lift.style.bottom='52.4%';
       break;
+      
     case 'level5':
       if(position == 33.4 || position == 52.4)
       lift.style.transitionDuration = t1;
@@ -51,6 +55,7 @@ function moveLift(el){
       lift.style.transitionDuration= t2;
       lift.style.bottom='42.7%';
       break;
+      
     case 'level4':
       if(position == 23.8 || position == 42.7)
       lift.style.transitionDuration = t1;
@@ -58,6 +63,7 @@ function moveLift(el){
       lift.style.transitionDuration= t2;
       lift.style.bottom='33.4%';
       break;
+      
     case 'level3':
       if(position == 33.4 || position == 14.6)
       lift.style.transitionDuration =t1;
@@ -65,6 +71,7 @@ function moveLift(el){
       lift.style.transitionDuration= t2;
       lift.style.bottom='23.8%';
       break;
+      
     case 'level2':
       if(position == 5.3 || position == 23.8)
       lift.style.transitionDuration = t1;
@@ -72,6 +79,7 @@ function moveLift(el){
       lift.style.transitionDuration= t2;
       lift.style.bottom='14.6%';
     break;
+      
     case 'level1':
       if(position == 14.6)
       lift.style.transitionDuration = t1;
