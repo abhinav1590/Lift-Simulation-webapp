41b5b5bebd4ff71f45c43a8abfdeaa12fa058714
Abhinav Mishra <86315672+abhinav1590@users.noreply.github.com>
2021-09-09 01:17:38 +0530

    Update lift.js

--- lift.js
+++ lift.js
@@ -4,6 +4,10 @@ const lift2= document.getElementById('lift_3');
 
 const callBtn= document.querySelectorAll('button');
 let counter = true;
+var trans = 2;
+
+function liftTrans(lift_id,pos1,pos2){
+lift_id.style.transitionDuration = (trans*Math.abs((pos2 - pos1)/10)).toString()+'s';}
 
 function moveLift(el){
 
@@ -21,76 +25,102 @@ function moveLift(el){
   switch(el.id){
 
     case 'level10':
-      lift.style.bottom = '90.8%';
+      liftTrans(lift,95,position);
+      lift.style.bottom = '95%';
       break;
 
     case 'level9':
-      lift.style.bottom='81.4%';
+      liftTrans(lift,85,position);
+      lift.style.bottom='85%';
       break;
 
     case 'level8':
-      if(position >81.4 && position1 >=52.4)
-      lift1.style.bottom='71.7%';
+      liftTrans(lift,75,position);
+      liftTrans(lift1,75,position1);
+      liftTrans(lift2,75,position2);
+      if(position >85 && position1 >=55)
+      lift1.style.bottom='75%';
       else
-      lift.style.bottom='71.7%';
+      lift.style.bottom='75%';
       break;
 
     case 'level7':
-      if(position > 71.7 && position1 >= 42.7)
-      lift1.style.bottom = '62.2%';
+      liftTrans(lift,65,position);
+      liftTrans(lift1,65,position1);
+      liftTrans(lift2,65,position2);
+      if(position > 75 && position1 >= 45)
+      lift1.style.bottom = '65%';
       else
-      lift.style.bottom='62.2%';
+      lift.style.bottom='65%';
       break;
 
     case 'level6':
-      if(position >62.2 && position1 >= 33.4 
-        && position1 <= 71.7 && position2!= 42.7)
-      lift1.style.bottom = '52.4%';
-      else if(position1 >=71.7 && position2 >= 33.4)
-      lift2.style.bottom = '52.4%';
+      liftTrans(lift,55,position);
+      liftTrans(lift1,55,position1);
+      liftTrans(lift2,55,position2);
+      if(position >65&& position1 >= 35
+        && position1 <= 75 && position2!= 45)
+      lift1.style.bottom = '55%';
+      else if(position1 >=75 && position2 >= 35)
+      lift2.style.bottom = '55%';
       else
-      lift.style.bottom='52.4%';
+      lift.style.bottom='55%';
       break;
 
     case 'level5':
-      if(position > 52.4 && position1 >= 23.8 && position1 <= 62.2)
-      lift1.style.bottom = '42.7%';
-      else if(position1 >=62.2 && position2 >= 23.8)
-      lift2.style.bottom ='42.7%';
+      liftTrans(lift,45,position);
+      liftTrans(lift1,45,position1);
+      liftTrans(lift2,45,position2);
+      if(position > 55 && position1 >= 25 && position1 <= 65)
+      lift1.style.bottom = '45%';
+      else if(position1 >=65 && position2 >= 25)
+      lift2.style.bottom ='45%';
       else
-      lift.style.bottom='42.7%';
+      lift.style.bottom='45%';
       break;
 
     case 'level4':
-      if( position >= 42.7 && position1 < 52.4)
-      lift1.style.bottom='33.4%';
-      else if(position1 >= 52.4)
-      lift2.style.bottom='33.4%';
+      liftTrans(lift,35,position);
+      liftTrans(lift1,35,position1);
+      liftTrans(lift2,35,position2);
+      if( position >= 45 && position1 < 55)
+      lift1.style.bottom='35%';
+      else if(position1 >= 55)
+      lift2.style.bottom='35%';
       else
-      lift.style.bottom= '33.4%';
+      lift.style.bottom= '35%';
       break;
       
     case 'level3':
-      if(position >= 42.7 && position1 <42.7)
-      lift1.style.bottom = '23.8%';
-      else if(position1 >=42.7)
-      lift2.style.bottom = '23.8%';
+      liftTrans(lift,25,position);
+      liftTrans(lift1,25,position1);
+      liftTrans(lift2,25,position2);
+      if(position >= 45 && position1 <45)
+      lift1.style.bottom = '25%';
+      else if(position1 >=45)
+      lift2.style.bottom = '25%';
       else
-      lift.style.bottom='23.8%';
+      lift.style.bottom='25%';
       break;
 
     case 'level2':
-      if(position >= 33.4 && position1 < 33.4)
-      lift1.style.bottom= '14.6%';
-      else if(position1 >= 33.4)
-      lift2.style.bottom = '14.6%';
+      liftTrans(lift,15,position);
+      liftTrans(lift1,15,position1);
+      liftTrans(lift2,15,position2);
+      if(position >= 35 && position1 < 35)
+      lift1.style.bottom= '15.0%';
+      else if(position1 >= 35)
+      lift2.style.bottom = '15.0%';
       else
-      lift.style.bottom='14.6%';
+      lift.style.bottom='15.0%';
       break;
     case 'level1':
-      lift2.style.bottom = '5.3%';
-      lift1.style.bottom = '5.3%';
-      lift.style.bottom = '5.3%';
+      liftTrans(lift,5,position);
+      liftTrans(lift1,5,position1);
+      liftTrans(lift2,5,position2);
+      lift2.style.bottom = '5.0%';
+      lift1.style.bottom = '5.0%';
+      lift.style.bottom = '5.0%';
       break;
         
   }
