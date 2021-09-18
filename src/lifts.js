c92d7bb27f928393db0b361fcd2a87d7cd8e9d7e
Abhinav Mishra <mabhinav534@gmail.com>
2021-09-18 20:32:31 +0530

    added

---
+++ src/lifts.js
@@ -0,0 +1,5 @@
+// All lift come here
+export const lift= document.getElementById('lift_1');
+export const lift1=document.getElementById('lift_2');
+export const lift2= document.getElementById('lift_3');
+export const lift3= document.getElementById('lift_4');
