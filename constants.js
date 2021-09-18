71d489b6248b879d36c7d6f65a36991309ebc3e7
Abhinav Mishra <mabhinav534@gmail.com>
2021-09-18 20:34:17 +0530

    deleted

--- constants.js
+++
@@ -1,4 +0,0 @@
-// all constants come here other than lift
-export const slider = document.getElementById("transitionRange");
-export const output = document.getElementById("range");
-export const callBtn= document.querySelectorAll('button');
--- lifts.js
+++
@@ -1,5 +0,0 @@
-// All lift come here
-export const lift= document.getElementById('lift_1');
-export const lift1=document.getElementById('lift_2');
-export const lift2= document.getElementById('lift_3');
-export const lift3= document.getElementById('lift_4');
