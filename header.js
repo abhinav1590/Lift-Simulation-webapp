510df97067f86b2ed900ba41399fd09aa51c5fdb
Abhinav Mishra <mabhinav534@gmail.com>
2021-09-19 16:10:51 +0530

    small changes

---
+++ header.js
@@ -0,0 +1,7 @@
+function over(){
+    document.getElementById("eg").innerHTML = "Hello There !";
+}
+function out() {
+    document.getElementById("eg").innerHTML = "LIFT SIMULATION";
+    window.location.reload();
+}
